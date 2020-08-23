fc65bdb64a0ec16eda04bac61e10daacf43c69bd
Mike MacHenry <mike.machenry@gmail.com>
2020-08-23 16:33:58 -0400

    Remove unneeded console logs

--- src/main.js
+++ src/main.js
@@ -11,16 +11,12 @@ const auth0 = new Auth0Client({
 });
 
 window.onload = async () => {
-    console.log("onload")
     const isAuthenticated = await auth0.isAuthenticated()
     if (isAuthenticated) {
-        console.log("authed")
         updateApp()
     } else {
-        console.log("not authed")
         const query = window.location.search;
         if (query.includes("code=") && query.includes("state=")) {
-            console.log("handler redirect")
             await auth0.handleRedirectCallback();
             updateApp()
             window.history.replaceState({}, document.title, "/");
@@ -29,20 +25,16 @@ window.onload = async () => {
 }
 
 const updateApp = async () => {
-    console.log("updateApp")
     const isAuthenticated = await auth0.isAuthenticated();
-    console.log("isAuthenticated",isAuthenticated)
     if (isAuthenticated) {
         const user = await auth0.getUser()
         const token = await auth0.getTokenSilently()
-        console.log("user",user,"token",token)
         app.ports.updateUserAndToken.send(token)
         //app.ports.updateUserAndToken.send({user: user, token: token})
     }
 }
 
 app.ports.login.subscribe(async (b) => {
-    console.log("login")
     await auth0.loginWithRedirect({
         redirect_uri: window.location.origin
     })
